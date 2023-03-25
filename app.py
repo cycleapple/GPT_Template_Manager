6fcd42aca87c8124ed8256c1f250c289a88da9b9
CY <cycleapple@gmail.com>
2023-03-25 21:04:07 +0800

    update data

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from markdown import markdown
+import re
+from markupsafe import Markup
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile('config.py')
@@ -9,15 +12,31 @@ db = SQLAlchemy(app)
 class Block(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    prompt = db.Column(db.String(200), nullable=False)
+    prompt = db.Column(db.String(2000), nullable=False)
     prompt_description = db.Column(db.String(200), nullable=False)
+    category = db.Column(db.String(50), nullable=False)
 
     def __repr__(self):
         return '<Block %r>' % self.title
 
+
 # create the tables in the database
 with app.app_context():
     db.create_all()
+
+# add markdown filter to the environment
+app.jinja_env.filters['markdown'] = markdown
+
+@app.template_filter()
+def highlight_brackets(text):
+    # define the regular expression pattern to match text within square brackets
+    pattern = r'\[([^\]]+)\]'
+    # compile the pattern into a regular expression object
+    regex = re.compile(pattern)
+    # replace each matched pattern with the same text without brackets, highlighted with a blue background
+    highlighted_text = regex.sub(r'<span style="background-color: #a0c4ff; color: black; border-radius: 4px; padding: 2px 4px;">\1</span>', text)
+    return Markup(highlighted_text)
+
 
 @app.route('/')
 def index():
